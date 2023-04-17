42e41be5b1f1ca72780a15da59cb11ad31b59755
IamLam163 <olamidealiyu@gmail.com>
2023-04-17 16:12:22 +0100

    returning the page details

---
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python3
+"""
+function returns the start and end index of a page
+"""
+
+
+import csv
+from typing import Dict, List, Tuple
+import math
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """function returns a tuple containing the start & end idx"""
+    start_idx = (page - 1) * page_size
+    end_idx = start_idx + page_size
+    return (start_idx, end_idx)
+
+
+class Server:
+    """class to paginate a database of popular baby names"""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self) -> None:
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """get page based on the csv file"""
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start >= len(data):
+            return []
+        return data[start:end]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        """returns a Dictionary"""
+        page_data = self.get_page(page, page_size)
+        total_pages = math.ceil(len(page_data) / page_size)
+        page_details = {
+            'page_size': len(page_data),
+            'page': page,
+            'data': page_data,
+            'next-page': None if page >= total_pages else page + 1,
+            'prev_page': None if page - 1 == 0 else page - 1,
+            'total_pages': total_pages,
+        }
+        return page_details
