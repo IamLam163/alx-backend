ca0e5f63d3c9a29255ff9660869c1bd7d13f3196
IamLam163 <olamidealiyu@gmail.com>
2023-04-17 15:07:01 +0100

    generating data based on pages supplied

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+function returns the start and end index of a page
+"""
+import csv
+from typing import List, Tuple
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """function returns a tuple containing the start & end idx"""
+    start_idx = (page - 1) * page_size
+    end_idx = start_idx + page_size
+    return (start_idx, end_idx)
+
+class Server:
+    """class to paginate a database of popular baby names"""
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self) -> None:
+        self.__dataset = None
+
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert type(page) == int and type(page_size) == int
+        assert page > 0 and page_size > 0
+        start, end = index_range(page, page_size)
+        data = self.dataset()
+        if start >= len(data):
+            return []
+        return data[start:end]
