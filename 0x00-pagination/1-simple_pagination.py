55cad993f33cdc66574e4d7e173b36f92b7a0203
IamLam163 <olamidealiyu@gmail.com>
2023-04-17 15:18:45 +0100

    pycodestyle

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,7 +8,6 @@ import csv
 from typing import List, Tuple
 
 
-
 def index_range(page: int, page_size: int) -> Tuple:
     """function returns a tuple containing the start & end idx"""
     start_idx = (page - 1) * page_size
@@ -23,7 +22,6 @@ class Server:
     def __init__(self) -> None:
         self.__dataset = None
 
-
     def dataset(self) -> List[List]:
         """Cached dataset"""
         if self.__dataset is None:
@@ -31,12 +29,12 @@ class Server:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
             self.__dataset = dataset[1:]
-        return self.__dataset
 
+        return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """get page based on the csv file"""
-        assert type(page) == int and type(page_size) == int
+        assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
         start, end = index_range(page, page_size)
         data = self.dataset()
