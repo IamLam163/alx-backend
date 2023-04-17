7afae21100cbfcad3658d5755b12ef86d1a11f31
IamLam163 <olamidealiyu@gmail.com>
2023-04-17 16:35:18 +0100

    removed test files

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -56,15 +56,3 @@ class Server:
             'total_pages': total_pages,
         }
         return page_details
-
-
-
-server = Server()
-
-print(server.get_hyper(1, 2))
-print("---")
-print(server.get_hyper(2, 2))
-print("---")
-print(server.get_hyper(100, 3))
-print("---")
-print(server.get_hyper(3000, 100))
