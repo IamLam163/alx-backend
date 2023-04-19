a9bddd6be3aa5254b67b965a8c77ab77d17a49a3
IamLam163 <olamidealiyu@gmail.com>
2023-04-19 23:31:08 +0100

    using a double ended queue deque

---
+++ 0x01-caching/33-lru_cache.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+"""
+class LRUCache that inherits from BaseCaching and is a caching system
+"""
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """class LRUCache that inherits from BaseCaching and is a 
+    caching system"""
+
+    def __init__(self):
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item
+        value for the key key"""
+        if key is None or item is None:
+            return
+
+        elif (key in self.cache_data):
+            self.order.remove(key)
+            self.order.append(key)
+            self.cache_data[key] = item
+        if len(self.cache_data) < BaseCaching.MAX_ITEMS:
+            self.order.append(key)
+            self.cache_data[key] = item
+        else:
+            last_elm = self.order.pop(0)
+            del self.cache_data[last_elm]
+            print("DISCARD: {}".format(last_elm))
+            self.order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns the value of the item with the
+        specified key from the cache."""
+        if key is None or key not in self.cache_data:
+            return None
+        if (key in self.order):
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data.get(key)
---
+++ 0x01-caching/deque.py
@@ -0,0 +1,68 @@
+from collections import deque
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """class LRUCache that inherits from BaseCaching and is a 
+    caching system"""
+
+    def __init__(self):
+        super().__init__()
+        self.order = deque()
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data the item
+        value for the key key"""
+        if key is None or item is None:
+            return
+
+        elif key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            self.cache_data[key] = item
+        elif len(self.cache_data) < BaseCaching.MAX_ITEMS:
+            self.order.append(key)
+            self.cache_data[key] = item
+        else:
+            last_elm = self.order.popleft()
+            del self.cache_data[last_elm]
+            print("DISCARD: {}".format(last_elm))
+            self.order.append(key)
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Returns the value of the item with the
+        specified key from the cache."""
+        if key is None or key not in self.cache_data:
+            return None
+        if key in self.order:
+            self.order.remove(key)
+            self.order.append(key)
+        return self.cache_data.get(key)
+
+my_cache = LRUCache()
+my_cache.put("A", "Hello")
+my_cache.put("B", "World")
+my_cache.put("C", "Holberton")
+my_cache.put("D", "School")
+my_cache.print_cache()
+print(my_cache.get("B"))
+my_cache.put("E", "Battery")
+my_cache.print_cache()
+my_cache.put("C", "Street")
+my_cache.print_cache()
+print(my_cache.get("A"))
+print(my_cache.get("B"))
+print(my_cache.get("C"))
+my_cache.put("F", "Mission")
+my_cache.print_cache()
+my_cache.put("G", "San Francisco")
+my_cache.print_cache()
+my_cache.put("H", "H")
+my_cache.print_cache()
+my_cache.put("I", "I")
+my_cache.print_cache()
+my_cache.put("J", "J")
+my_cache.print_cache()
+my_cache.put("K", "K")
+my_cache.print_cache()
