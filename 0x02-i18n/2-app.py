99c03ca68c145370ac7f13ce3e2890b26e05bd35
IamLam163 <olamidealiyu@gmail.com>
2023-04-26 05:59:42 +0100

    fix

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+"""
+Basic Babel Setup
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+import babel
+
+
+class Config:
+    """Flask Babel Configuration"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """get locale from the user"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route('/')
+def render_index() -> str:
+    """renders 2-index.html"""
+    return render_template('2-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
